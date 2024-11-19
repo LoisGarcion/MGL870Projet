94cb189fa6854cb194807475982ccdbb2d9885e5
loisg <lois.garcion34@gmail.com>
2024-11-18 23:08:55 -0500

    Setup de base du service User

---
+++ MicroUser/UserSpringBoot/src/test/java/MicroUser/UserSpringBoot/UserSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroUser.UserSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
